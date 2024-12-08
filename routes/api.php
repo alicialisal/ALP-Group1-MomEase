5d5187731963ede69826c9ad857a2fd520c6b3ea
Apryadi D. Putra Tangalayuk <putracraft21@gmail.com>
2024-12-08 23:16:11 +0800

    Menambahkan API untuk Profile dan Update Profile

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -16,3 +17,7 @@ Route::post("register",[AuthController::class,"register"]);
 Route::post("login",[AuthController::class,"login"]);
 //posts
 Route::apiResource('/journaling', App\Http\Controllers\Api\MoodJournalingController::class);
+
+Route::get('/profiles', [ProfileController::class, 'index']);
+Route::put('/profiles/{id}', [ProfileController::class, 'update']);
+
