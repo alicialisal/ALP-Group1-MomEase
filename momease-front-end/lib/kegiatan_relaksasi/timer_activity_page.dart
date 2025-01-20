90c95a8fc292aab354d9143b5aa03df5d2c1f453
Javin Erasmus Clementino <javinpro12@gmail.com>
2025-01-20 08:41:18 +0800

    Timer Countdown done
    - Step Countdown (on progress)
    - Modal conditional utk perpindahan (on progress)

--- momease-front-end/lib/kegiatan_relaksasi/timer_activity_page.dart
+++ momease-front-end/lib/kegiatan_relaksasi/timer_activity_page.dart
@@ -1,169 +1,143 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-class TimerActivityPage extends StatefulWidget {
-  final String duration;
+class RelaxationTimerPage extends StatefulWidget {
+  final Map<String, dynamic> activity;
 
-  TimerActivityPage({required this.duration});
+  const RelaxationTimerPage({Key? key, required this.activity})
+      : super(key: key);
 
   @override
-  _TimerActivityPageState createState() => _TimerActivityPageState();
+  _RelaxationTimerPageState createState() => _RelaxationTimerPageState();
 }
 
-class _TimerActivityPageState extends State<TimerActivityPage> {
-  late int _remainingTime;
-  late Timer _timer;
-  bool _isPaused = false;
+class _RelaxationTimerPageState extends State<RelaxationTimerPage> {
+  late int totalSeconds;
+  late int remainingSeconds;
+  Timer? _timer;
+  bool isPaused = false;
 
   @override
   void initState() {
     super.initState();
-    // Parse durasi dari string menjadi detik
-    _remainingTime = _parseDuration(widget.duration);
+    totalSeconds = _parseDurationToSeconds(widget.activity['duration']);
+    remainingSeconds = totalSeconds;
     _startTimer();
   }
 
-  // Fungsi untuk mengkonversi format mm:ss ke dalam detik
-  int _parseDuration(String duration) {
-    final parts = duration.split(':');
-    if (parts.length == 2) {
-      try {
-        int minutes = int.parse(parts[0]);
-        int seconds = int.parse(parts[1]);
-        return minutes * 60 + seconds; // Mengkonversi menjadi detik
-      } catch (e) {
-        print('Error parsing duration: $e');
-        return 0;
-      }
-    } else {
-      print('Invalid duration format');
-      return 0;
-    }
+  int _parseDurationToSeconds(String duration) {
+    final parts = duration.split(' ');
+    final minutes = int.parse(parts[0]);
+    return minutes * 60;
   }
 
-  // Fungsi untuk memulai timer yang akan menghitung mundur setiap detik
   void _startTimer() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      if (_remainingTime > 0 && !_isPaused) {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (!isPaused && remainingSeconds > 0) {
         setState(() {
-          _remainingTime--;
+          remainingSeconds--;
         });
-      } else if (_remainingTime == 0) {
-        _timer.cancel(); // Menghentikan timer ketika waktu mencapai 0
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text("Time's Up!"),
-              content: Text("Your time has ended."),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text('OK'),
-                ),
-              ],
-            );
-          },
-        );
+      } else if (remainingSeconds <= 0) {
+        _timer?.cancel();
       }
     });
   }
 
-  // Fungsi untuk memformat waktu dalam format mm:ss
-  String _formatTime(int time) {
-    int minutes = time ~/ 60;
-    int seconds = time % 60;
-    return '$minutes:${seconds.toString().padLeft(2, '0')}';
+  void _pauseTimer() {
+    setState(() {
+      isPaused = true;
+    });
+  }
+
+  void _resumeTimer() {
+    setState(() {
+      isPaused = false;
+    });
+  }
+
+  void _exitActivity() {
+    _timer?.cancel();
+    Navigator.pop(context);
+  }
+
+  String _formatTime(int seconds) {
+    final minutes = (seconds / 60).floor();
+    final remainingSeconds = seconds % 60;
+    return '${minutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}';
   }
 
   @override
   void dispose() {
-    _timer.cancel();
+    _timer?.cancel();
     super.dispose();
-  }
-
-  // Fungsi untuk menjeda atau melanjutkan timer
-  void _pauseTimer() {
-    setState(() {
-      _isPaused = !_isPaused;
-    });
-  }
-
-  // Fungsi untuk keluar dari aktivitas dan kembali ke halaman sebelumnya
-  void _exitActivity() {
-    Navigator.pop(context); // Kembali ke halaman sebelumnya
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Activity Timer'),
+        title: Text(
+          widget.activity['title'],
+          style: TextStyle(color: Color(0xFFFFFFFF)),
+        ),
+        backgroundColor: widget.activity['categoryColor'],
         centerTitle: true,
-        backgroundColor: Color(0xff6495ED),
+        iconTheme: IconThemeData(color: Colors.white),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // Menampilkan timer
-            Container(
-              padding: EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              child: Text(
-                _formatTime(
-                  _remainingTime,
-                ), // Menampilkan waktu dalam format mm:ss
-                style: TextStyle(
-                  fontSize: 48.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Remaining Time',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-              ),
+                Text(
+                  _formatTime(remainingSeconds),
+                  style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
-            SizedBox(height: 20.0),
-
-            // Tombol Pause/Resume
-            ElevatedButton(
-              onPressed: _pauseTimer,
-              child: Text(
-                _isPaused ? 'Resume' : 'Pause',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 12.0),
-                backgroundColor: Colors.orange,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-              ),
+            Text(
+              widget.activity['description'],
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16),
             ),
-            SizedBox(height: 20.0),
-
-            // Tombol Exit
-            ElevatedButton(
-              onPressed: _exitActivity,
-              child: Text(
-                'Exit Activity',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white,
+            Spacer(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(width: double.infinity),
+                ElevatedButton(
+                  onPressed: isPaused ? _resumeTimer : _pauseTimer,
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                  child: Text(
+                    isPaused ? 'Resume' : 'Pause',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
                 ),
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 12.0),
-                backgroundColor: Colors.red,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25.0),
+                ElevatedButton(
+                  onPressed: _exitActivity,
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                  child: const Text(
+                    'Exit Activity',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
           ],
         ),
