59781bd218a561a492f797edeca5fe89402e4f0f
Javin Erasmus Clementino <javinpro12@gmail.com>
2025-01-21 09:40:25 +0800

    Completed dialog modal
    Dialog pop up saat ingin kembali ke page sebelumnya sudah selesai dibuat

--- momease-front-end/lib/kegiatan_relaksasi/timer_activity_page.dart
+++ momease-front-end/lib/kegiatan_relaksasi/timer_activity_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:front_end/kegiatan_relaksasi/search_page.dart';
 import 'dart:async';
+import 'dart:ui';
 
 class RelaxationTimerPage extends StatefulWidget {
   final Map<String, dynamic> activity;
@@ -55,11 +57,6 @@ class _RelaxationTimerPageState extends State<RelaxationTimerPage> {
     });
   }
 
-  void _exitActivity() {
-    _timer?.cancel();
-    Navigator.pop(context);
-  }
-
   String _formatTime(int seconds) {
     final minutes = (seconds / 60).floor();
     final remainingSeconds = seconds % 60;
@@ -74,72 +71,304 @@ class _RelaxationTimerPageState extends State<RelaxationTimerPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.activity['title'],
-          style: TextStyle(color: Color(0xFFFFFFFF)),
+    return WillPopScope(
+      onWillPop: () async {
+        // Menampilkan dialog saat tombol back device ditekan
+        final shouldPop = await showDialog<bool>(
+          context: context,
+          builder: (BuildContext context) {
+            return BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5), // Blur background
+              child: AlertDialog(
+                title: Text(
+                  'Are you sure want to quit this activity?',
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 27),
+                ),
+                content: Text(
+                  'Are you willing to go back?',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF808080),
+                  ),
+                ),
+                actions: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.pop(context, false); // Tidak keluar
+                          },
+                          child: Text(
+                            'No, I don\'t',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Expanded(
+                        flex: 1,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context, true); // Keluar
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red,
+                            foregroundColor: Colors.white,
+                            padding: const EdgeInsets.all(10),
+                          ),
+                          child: Text(
+                            'Yes, I do',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+        return shouldPop ?? false; // Keluar hanya jika 'Yes, I do' dipilih
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            widget.activity['title'],
+            style: TextStyle(color: Color(0xFFFFFFFF)),
+          ),
+          backgroundColor: widget.activity['categoryColor'],
+          centerTitle: true,
+          iconTheme: IconThemeData(color: Colors.white),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 5,
+                      sigmaY: 5,
+                    ), // Blur background
+                    child: AlertDialog(
+                      title: Text(
+                        'Are you sure want to quit this activity?',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 27,
+                        ),
+                      ),
+                      content: Text(
+                        'Are you willing to go back?',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF808080),
+                        ),
+                      ),
+                      actions: [
+                        Row(
+                          mainAxisAlignment:
+                              MainAxisAlignment.end, // Posisi tombol di kanan
+                          children: [
+                            Expanded(
+                              flex: 1, // Proporsi lebar tombol
+                              child: TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context); // Close dialog
+                                },
+                                child: Text(
+                                  'No, I don\'t',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 8), // Jarak antar tombol
+                            Expanded(
+                              flex: 1, // Proporsi lebar tombol
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.pop(context); // Close dialog
+                                  Navigator.pop(
+                                    context,
+                                  ); // Navigate to previous page
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.red,
+                                  foregroundColor: Colors.white,
+                                  padding: const EdgeInsets.all(10),
+                                ),
+                                child: Text(
+                                  'Yes, I do',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+          ),
         ),
-        backgroundColor: widget.activity['categoryColor'],
-        centerTitle: true,
-        iconTheme: IconThemeData(color: Colors.white),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Remaining Time',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  _formatTime(remainingSeconds),
-                  style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            Text(
-              widget.activity['description'],
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16),
-            ),
-            Spacer(),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(width: double.infinity),
-                ElevatedButton(
-                  onPressed: isPaused ? _resumeTimer : _pauseTimer,
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                  child: Text(
-                    isPaused ? 'Resume' : 'Pause',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Remaining Time',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    _formatTime(remainingSeconds),
+                    style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Text(
+                widget.activity['description'],
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(width: double.infinity),
+                  ElevatedButton(
+                    onPressed: isPaused ? _resumeTimer : _pauseTimer,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+                    child: Text(
+                      isPaused ? 'Resume' : 'Pause',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: _exitActivity,
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                  child: const Text(
-                    'Exit Activity',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
+                  ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        barrierDismissible:
+                            true, // Background dapat di-klik untuk menutup dialog
+                        builder: (BuildContext context) {
+                          return BackdropFilter(
+                            filter: ImageFilter.blur(
+                              sigmaX: 5,
+                              sigmaY: 5,
+                            ), // Blur background
+
+                            child: AlertDialog(
+                              title: Text(
+                                'Are you sure wanna quit?',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 27,
+                                ),
+                              ),
+                              content: Text(
+                                'We\'ll take you back to list activities',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xFF808080),
+                                ),
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context); // Tutup dialog
+                                  },
+                                  child: Text(
+                                    'No, I don\'t',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.pop(context); // Tutup dialog
+                                    Navigator.pushAndRemoveUntil(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => RelaxationApp(),
+                                      ),
+                                      (route) =>
+                                          false, // Menghapus semua rute sebelumnya
+                                    );
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.red,
+                                    foregroundColor: Colors.white,
+                                  ),
+                                  child: Text(
+                                    'Yes, I want',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                    ),
+                    child: const Text(
+                      'Exit Activity',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
